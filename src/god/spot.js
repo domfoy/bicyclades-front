0ff95985c055b73ed026b54a44aaecd6be8679a2
domfoy <quentin.pergeline@hotmail.fr>
2019-03-24 15:37:01 +0100

    fix(god tiles): center numbers in offering spots

--- src/god/spot.js
+++ src/god/spot.js
@@ -19,19 +19,25 @@ class GodMarkerSpot extends Component {
       .getChildByName('godsContainer')
       .getChildByName(`godContainer${props.godId}`);
 
+    const center = {
+      x: props.x + props.size / 2,
+      y: props.y + props.size / 2
+    };
+
     const tile = new Pixi.Graphics();
 
     tile.beginFill(0xFF0000);
     tile.drawCircle(0, 0, props.size / 2);
     tile.endFill();
 
-    tile.x = props.x + props.size / 2;
-    tile.y = props.y + props.size / 2;
+    tile.x = center.x;
+    tile.y = center.y;
     godContainer.addChild(tile);
 
     const spotText = new Pixi.Text(`${id}`, {fontFamily : 'Arial', fontSize: props.size / 2, fill : 0xffffff, align : 'center'});
-    spotText.x = props.x;
-    spotText.y = props.y;
+    spotText.anchor.set(0.5, 0.5);
+    spotText.x = center.x;
+    spotText.y = center.y;
     godContainer.addChild(spotText);
   }
 }
