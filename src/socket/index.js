ad4426249a646eccc4ece2316b3098af10617ba7
domfoy <quentin.pergeline@hotmail.fr>
2019-05-01 19:19:14 +0200

    feat(socket): add "add name" message

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,19 +1,39 @@
 import _ from 'lodash';
 
-import gameActions from '../game';
+// import gameActions from '../game';
 import {createProtoMessage, parseProtoMessage} from './proto';
 
 export default (url = 'ws://localhost:9002/ws') => (storeApi) => {
-  const socket = new WebSocket(url);
+  const socket1 = new WebSocket(url);
 
-  socket.binaryType = 'arraybuffer';
+  socket1.binaryType = 'arraybuffer';
 
-  socket.onopen = () => {
+  function sendToServer(socket, payload) {
+    if (socket.readyState === WebSocket.OPEN) {
+      console.log('about to send data to server');
+      const buffer = createProtoMessage(payload);
+
+      socket.send(buffer);
+      console.log('message sent to server');
+    } else {
+      console.log('could not send message');
+    }
+  }
+
+  socket1.onopen = () => {
     console.log('Connected');
-    storeApi.dispatch(gameActions.joinGame());
+    // storeApi.dispatch(gameActions.joinGame());
+    sendToServer(socket1, {
+      globalAction: 'CLIENT_ACTION',
+      clientAction: {
+        type: 'ADD_NAME',
+        name: 'p1'
+      }
+    });
+    // socket.close();
   };
 
-  socket.onclose = () => {
+  socket1.onclose = () => {
     console.log('Closed');
     storeApi.dispatch({
       type: 'ACK_CLOSE_SOCKET',
@@ -24,24 +44,14 @@ export default (url = 'ws://localhost:9002/ws') => (storeApi) => {
     });
   };
 
-  socket.onmessage = (evt) => {
+  socket1.onmessage = (evt) => {
     try {
       const object = parseProtoMessage(evt.data);
-      console.log('Received from server', JSON.stringify(object, null, 2));
+      console.log('Received from server for p1', JSON.stringify(object, null, 2));
     } catch (err) {
       throw err;
     }
   };
-
-  function sendToServer(payload) {
-    if (socket.readyState === WebSocket.OPEN) {
-      const buffer = createProtoMessage(payload);
-
-      socket.send(buffer);
-    } else {
-      console.log('could not send message');
-    }
-  }
 
   return next => (action) => {
     if (_.get(action, 'meta.isSocket') && _.get(action, 'meta.direction') !== 'DOWN') {
--- src/socket/proto/index.js
+++ src/socket/proto/index.js
@@ -25,5 +25,5 @@ export function parseProtoMessage(data) {
 }
 
 export function createProtoMessage(payload) {
-  return Action.encode(payload).finish();
+  return Action.encode(Action.formObject(payload)).finish();
 }
